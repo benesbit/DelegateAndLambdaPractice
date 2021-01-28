43730596dea1baff486c4374fc020daf0de7e0bc
Ben Nesbit <benesbit@yahoo.com>
2021-01-28 11:03:20 -0800

    Comment out old code - prepare to use Worker class.
    Will be using delegates in a better way, comment out the old way. Do not remove yet.

--- DelegatesAndLambdas/Program.cs
+++ DelegatesAndLambdas/Program.cs
@@ -7,40 +7,42 @@ namespace DelegatesAndLambdas
     {
         static void Main(string[] args)
         {
-            WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
-            WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
-            WorkPerformedHandler del3 = new WorkPerformedHandler(WorkPerformed3);
+            //WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
+            //WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+            //WorkPerformedHandler del3 = new WorkPerformedHandler(WorkPerformed3);
 
-            del1 += del2 + del3;
+            //del1 += del2 + del3;
 
-            int finalHours = del1(10, WorkType.GenerateReports);
-            Console.WriteLine($"Final hours: {finalHours}");
+            //int finalHours = del1(10, WorkType.GenerateReports);
+            //Console.WriteLine($"Final hours: {finalHours}");
+
+            var worker = new Worker();
 
             Console.Read();
         }
 
-        static void DoWork(WorkPerformedHandler del)
-        {
-            del(5, WorkType.GoToMeetings);
-        }
+        //static void DoWork(WorkPerformedHandler del)
+        //{
+        //    del(5, WorkType.GoToMeetings);
+        //}
 
-        static int WorkPerformed1(int hours, WorkType workType)
-        {
-            Console.WriteLine($"WorkPerformed1 Called: {hours} and {workType}");
-            return hours + 1;
-        }
+        //static int WorkPerformed1(int hours, WorkType workType)
+        //{
+        //    Console.WriteLine($"WorkPerformed1 Called: {hours} and {workType}");
+        //    return hours + 1;
+        //}
 
-        static int WorkPerformed2(int hours, WorkType workType)
-        {
-            Console.WriteLine($"WorkPerformed2 Called: {hours} and {workType}");
-            return hours + 2;
-        }
+        //static int WorkPerformed2(int hours, WorkType workType)
+        //{
+        //    Console.WriteLine($"WorkPerformed2 Called: {hours} and {workType}");
+        //    return hours + 2;
+        //}
 
-        static int WorkPerformed3(int hours, WorkType workType)
-        {
-            Console.WriteLine($"WorkPerformed3 Called: {hours} and {workType}");
-            return hours + 3;
-        }
+        //static int WorkPerformed3(int hours, WorkType workType)
+        //{
+        //    Console.WriteLine($"WorkPerformed3 Called: {hours} and {workType}");
+        //    return hours + 3;
+        //}
     }
 
     public enum WorkType
