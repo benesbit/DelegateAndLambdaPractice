639c9abe9771d6ceda6aa87f467401eee2e0e4ad
Ben Nesbit <benesbit@yahoo.com>
2021-01-28 09:39:02 -0800

    Initial Commit

---
+++ DelegatesAndLambdas/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DelegatesAndLambdas
+{
+    public delegate void WorkPerformedHandler(int hours, WorkType workType);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
+            WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+            WorkPerformedHandler del3 = new WorkPerformedHandler(WorkPerformed3);
+
+            del1 += del2;
+            del1 += del3;
+
+            del1(10, WorkType.GenerateReports);
+
+            Console.Read();
+        }
+
+        static void DoWork(WorkPerformedHandler del)
+        {
+            del(5, WorkType.GoToMeetings);
+        }
+
+        static void WorkPerformed1(int hours, WorkType workType)
+        {
+            Console.WriteLine($"WorkPerformed1 Called: {hours} and {workType}");
+        }
+
+        static void WorkPerformed2(int hours, WorkType workType)
+        {
+            Console.WriteLine($"WorkPerformed2 Called: {hours} and {workType}");
+        }
+
+        static void WorkPerformed3(int hours, WorkType workType)
+        {
+            Console.WriteLine($"WorkPerformed3 Called: {hours} and {workType}");
+        }
+    }
+
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports
+    }
+}
