e583b7aa1afed316e470e4c67b6bd59ffcc5210b
Ben Nesbit <benesbit@yahoo.com>
2021-01-28 10:18:07 -0800

    Move delegate to class that it is needed in.

--- DelegatesAndLambdas/Worker.cs
+++ DelegatesAndLambdas/Worker.cs
@@ -4,8 +4,12 @@ using System.Text;
 
 namespace DelegatesAndLambdas
 {
+    public delegate int WorkPerformedHandler(int hours, WorkType workType);
+
     class Worker
     {
+        public event 
+
         public void DoWork(int hours, WorkType workType)
         {
 
