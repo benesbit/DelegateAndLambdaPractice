206ebe129ab72ed26625d221c94c14dca6fd136b
Ben Nesbit <benesbit@yahoo.com>
2021-01-28 13:33:17 -0800

    Removed commented code.
    Commented code was there to show legacy way of doing thing, and show simpiler demonsstration of lesson. At this point, it is irrelevant and too confusing to see it all there. If needed, it is a part of source control and can be found again.

--- DelegatesAndLambdas/Program.cs
+++ DelegatesAndLambdas/Program.cs
@@ -7,15 +7,6 @@ namespace DelegatesAndLambdas
     {
         static void Main(string[] args)
         {
-            //WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
-            //WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
-            //WorkPerformedHandler del3 = new WorkPerformedHandler(WorkPerformed3);
-
-            //del1 += del2 + del3;
-
-            //int finalHours = del1(10, WorkType.GenerateReports);
-            //Console.WriteLine($"Final hours: {finalHours}");
-
             var worker = new Worker();
             worker.WorkPerformed += new EventHandler<WorkPerformedEventArgs>(Worker_WorkPerformed);
             worker.WorkCompleted += new EventHandler(Worker_WorkCompleted);
@@ -33,29 +24,6 @@ namespace DelegatesAndLambdas
         {
             Console.WriteLine("Worker is done!");
         }
-
-        //static void DoWork(WorkPerformedHandler del)
-        //{
-        //    del(5, WorkType.GoToMeetings);
-        //}
-
-        //static int WorkPerformed1(int hours, WorkType workType)
-        //{
-        //    Console.WriteLine($"WorkPerformed1 Called: {hours} and {workType}");
-        //    return hours + 1;
-        //}
-
-        //static int WorkPerformed2(int hours, WorkType workType)
-        //{
-        //    Console.WriteLine($"WorkPerformed2 Called: {hours} and {workType}");
-        //    return hours + 2;
-        //}
-
-        //static int WorkPerformed3(int hours, WorkType workType)
-        //{
-        //    Console.WriteLine($"WorkPerformed3 Called: {hours} and {workType}");
-        //    return hours + 3;
-        //}
     }
 
     public enum WorkType
