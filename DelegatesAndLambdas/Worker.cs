846ad4fb4d392a2e0bef0dad0b0be3fa3a895dcb
Ben Nesbit <benesbit@yahoo.com>
2021-01-28 10:20:13 -0800

    Create new events.
    Create event for work performed and event for work completed.

--- DelegatesAndLambdas/Worker.cs
+++ DelegatesAndLambdas/Worker.cs
@@ -8,7 +8,8 @@ namespace DelegatesAndLambdas
 
     class Worker
     {
-        public event 
+        public event WorkPerformedHandler WrokPerformed;
+        public event EventHandler WorkCompleted;
 
         public void DoWork(int hours, WorkType workType)
         {
