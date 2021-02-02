d09c46de141b695dd8f21e1df384a8e13845275e
Ben Nesbit <benesbit@yahoo.com>
2021-02-02 14:08:35 -0800

    Create static function for anonymous function delegate examples.
    Also added so text to make the program more user friendly.

--- DelegatesAndLambdas/Program.cs
+++ DelegatesAndLambdas/Program.cs
@@ -14,6 +14,8 @@ namespace DelegatesAndLambdas
 
             BasicDelegateExamples(hoursWorked);
 
+            BasicDelegateAnonymousFunctionExamples(hoursWorked);
+
             BizRulesDelegate addDel = (x, y) => x + y;
             BizRulesDelegate multiplyDel = (x, y) => x * y;
 
@@ -28,23 +30,27 @@ namespace DelegatesAndLambdas
             data.ProcessAction(firstValue, secondValue, myMultiplyAction);
         }
 
-        private static void BasicDelegateExamples(int hoursWorked)
+        private static void BasicDelegateAnonymousFunctionExamples(int hoursWorked)
         {
-            var worker = new Worker();
-            worker.WorkPerformed += Worker_WorkPerformed;
-            worker.WorkCompleted += Worker_WorkCompleted;
-            worker.DoWork(hoursWorked, WorkType.GenerateReports);
-        }
-
-        private void BasicDelegateAnonymousFunctionExamples(int first, int second)
-        {
+            Console.WriteLine($"---------- Begin Anonymous Function Delegate Example ----------");
             var worker = new Worker();
             worker.WorkPerformed += (s, e) =>
             {
                 Console.WriteLine($"{e.Hours} Hour" + ((e.Hours > 1) ? "s" : "") + $" worked\t Work Type: {e.WorkType}");
             };
             worker.WorkCompleted += (s, e) => Console.WriteLine("Worker is done!");
-            worker.DoWork(8, WorkType.GenerateReports);
+            worker.DoWork(hoursWorked, WorkType.GenerateReports);
+            Console.WriteLine($"---------- End Anonymous Function Delegate Example ----------\n\n");
+        }
+
+        private static void BasicDelegateExamples(int hoursWorked)
+        {
+            Console.WriteLine($"---------- Begin Basic Delegate Example ----------");
+            var worker = new Worker();
+            worker.WorkPerformed += Worker_WorkPerformed;
+            worker.WorkCompleted += Worker_WorkCompleted;
+            worker.DoWork(hoursWorked, WorkType.GenerateReports);
+            Console.WriteLine($"---------- End Basic Delegate Example ----------\n\n");
         }
 
         private static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
